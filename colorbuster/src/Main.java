5ec9763415015c287f904c953e85c5e462c8ca5e
Dayna-developer <61639668+Dayna-developer@users.noreply.github.com>
2020-03-02 12:55:49 -0500

    Add files via upload

---
+++ colorbuster/src/Main.java
@@ -0,0 +1,20 @@
+import controller.GameController;
+
+/**
+ * 
+ */
+
+/**
+ * @author Dayna Robinson
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+					
+			new GameController();
+
+	}
+}
+
+
